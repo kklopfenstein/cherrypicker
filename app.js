beee7ef2df66d849ba0fbdf195def92e7bad548f
Kevin Klopfenstein <kklopfenstein@gmail.com>
2015-05-25 20:57:01 -0400

    pieces saved to server

--- app.js
+++ app.js
@@ -7,10 +7,44 @@ server.listen(80);
 
 app.use(express.static('public'));
 
+var board = {};
+
+var ids = 0;
+
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  socket.on('add piece', function (data) {
+    addPiece(socket, data); 
   });
+
+  socket.on('get id', function (data) {
+    sendId(socket);
+  });
+  getState(socket);
 });
 
+function addPiece(socket, data) {
+  console.log("adding piece: " + data);
+  if (data !== undefined && data.id !== undefined
+      && data.x !== undefined && data.y !== undefined) {
+    console.log(data.x + ',' + data.y);
+    if (board[data.x] === undefined) {
+      board[data.x] = { }
+    }
+    board[data.x][data.y] = data;
+    socket.emit('success', { success: true });
+  } else {
+    socket.emit('success', { success: false });
+  }
+}
+
+function getState(socket) {
+  socket.on('get pieces', function (data) {
+    socket.emit('pieces', board);
+  });
+}
+
+function sendId(socket) {
+  socket.emit('send id', { id: ids });
+  ids++;
+}
--- public/js/game.js
+++ public/js/game.js
@@ -1,10 +1,30 @@
 var socket = io.connect('/');
+var myId = -1;
+
+var state = 0;
+var pieces;
+
 socket.on('news', function (data) {
   console.log(data);
   socket.emit('my other event', { my: 'data' });
 });
 
+socket.on('success', function (data) {
+  console.log('success=' + data.success);
+});
 
+socket.on('send id', function (data) {
+  myId = data.id;
+  console.log('client id=' + myId);
+});
+
+socket.on('pieces', function (data) {
+  console.log('found pieces...');
+  console.log(data);
+  pieces = data; 
+  clearBoard();
+  drawPieces();
+});
 // canvas
 
 var gameCanvas;
@@ -13,15 +33,24 @@ var bContext;
 var cherryRed = '#CC3300';
 var black = '#000';
 
-var cherryRadius = 25;
+var cherryRadius = 10;
+var dt = new Date();
+var rps = 1;
 
 function init() {
   gameCanvas = document.getElementById('GameCanvas');
   bContext = gameCanvas.getContext('2d');
   gameCanvas.addEventListener('click', gameOnClick, false);
-  gameCanvas.width = $(window).width();
-  gameCanvas.height = $(window).height(); 
+  gameCanvas.width = 640;
+  gameCanvas.height = 480; 
   // bContext.fillRect(50, 25, 150, 100);
+  $('#NameModal').modal();
+  socket.emit('get id', { });
+}
+
+function clearBoard() {
+  console.log('clearing canvas');
+  bContext.clearRect(0,0,gameCanvas.width,gameCanvas.height);
 }
 
 function drawCherry(x, y, radius) {
@@ -37,6 +66,7 @@ function drawCherry(x, y, radius) {
   bContext.stroke();
   bContext.fillStyle = cherryRed;
   bContext.fill();
+
 }
 
 function gameOnClick(e) {
@@ -44,6 +74,8 @@ function gameOnClick(e) {
 
   console.log("game click at " + pos.x + "," + pos.y);
 
+  socket.emit('add piece', { x: pos.x, y: pos.y, id: myId });
+ 
   drawCherry(pos.x, pos.y);
 }
 
@@ -66,7 +98,54 @@ function getCursorPosition(e) {
   return { x: x, y: y }
 }
 
+function getPieces() {
+  socket.emit('get pieces', { });
+}
+
+function drawPieces() {
+  console.log('drawing pieces');
+  if (pieces !== undefined) {
+    for (var pieceX in pieces) {
+      if (pieces.hasOwnProperty(pieceX)) {
+        for (var pieceY in pieces[pieceX]) {
+          if (pieces[pieceX].hasOwnProperty(pieceY)) {
+            p = pieces[pieceX][pieceY];
+            if (p.x !== undefined && p.y !== undefined) {
+              drawCherry(p.x, p.y, cherryRadius);
+            }
+          }
+        }
+      }
+    }
+  } else {
+    console.log('no pieces!');
+  }
+}
+
+function updateBoard() {
+  console.log("updating board");
+  reqInProgress = true;
+  getPieces();
+}
+
+function doLoop() {
+  var cd = new Date(); //current date
+  if ((cd.getTime() - dt.getTime()) >= 1000) {
+    console.log('updating board');
+    dt = cd;
+    updateBoard();
+  }
+}
+
+function gameLoop() {
+  if (state == 1)
+    doLoop();
+    requestAnimationFrame(gameLoop);
+}
+
 $(function() {
   init();
+  state = 1;
+  requestAnimationFrame(gameLoop);
 });
 
